8a79f9b1dad18bd732f56a01c7e18052e8488ced
Sena U <annes.iusu@gmail.com>
2018-08-18 13:08:30 +0900

    write php logics

--- calendar.php
+++ calendar.php
@@ -1,3 +1,50 @@
+<?php
+/*-----------------------
+ 日付の設定
+------------------------*/
+// 現在の年月
+$year  = date('Y');
+$month = date('m');
+// 当月の日数を取得
+$countDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+// 当月1日の曜日を取得
+$firstDay = date('w', strtotime($year . $month . '01'));
+// 当月最終日の曜日を取得
+$lastDay = date('w', strtotime($year . $month . $countDays));
+
+/*-----------------------
+ カレンダー出力
+------------------------*/
+// タイトル
+$title = $year . "年" . $month . "月";
+// カレンダー
+function printCalendar($firstDay, $lastDay, $countDays) {
+    $calendar = [];
+    // 月初の空白部分
+    for ($i = 1; $i < $firstDay + 2; $i++) {
+        $calendar[$i] = "<td></td>";
+    }
+    // 日付
+    $count = count($calendar);
+    for ($i = 0; $i < $countDays; $i++) {
+        $date = $i + 1;
+        $calendar[$count + $i] = "<td>" . $date . "</td>";
+    }
+    // 月末の空白部分
+    $countAll = count($calendar);
+    $lastBlank = 6 - $lastDay;
+    for ($i = 1; $i < $lastBlank + 1; $i++) {
+        $calendar[$countAll + $i] = "<td></td>";
+    }
+    // 出力
+    foreach ($calendar as $key => $val) {
+        echo $val;
+        if ($key % 7 == 0) {
+            echo "</tr><tr>";
+        }
+    }
+}
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +63,7 @@
     </style>
 </head>
 <body>
-    <div></div>
+    <p><?= $title; ?></p>
     <table rules="all" border="1" align="center">
         <tr>
             <td class="sun">SUN</td>
@@ -26,6 +73,9 @@
             <td>THU</td>
             <td>FRI</td>
             <td class="sat">SAT</td>
+        </tr>
+        <tr>
+            <?php printCalendar($firstDay, $lastDay, $countDays); ?>
         </tr>
     </table>
 </body>
