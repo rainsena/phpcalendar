33b1c4277aa3cbc8e3455757e4ee1dbc0f82ed67
Sena U <annes.iusu@gmail.com>
2018-08-18 12:45:31 +0900

    create calendar.php file

---
+++ calendar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Calendar</title>
+    <style>
+        body {
+            text-align: center;
+        }
+        .sun {
+            color: red;
+        }
+        .sat {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <div></div>
+    <table rules="all" border="1" align="center">
+        <tr>
+            <td class="sun">SUN</td>
+            <td>MON</td>
+            <td>TUE</td>
+            <td>WED</td>
+            <td>THU</td>
+            <td>FRI</td>
+            <td class="sat">SAT</td>
+        </tr>
+    </table>
+</body>
+</html>
